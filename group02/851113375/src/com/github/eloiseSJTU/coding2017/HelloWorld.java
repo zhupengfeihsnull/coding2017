7ffbd18021329aa1c04e59eaab56f3b5bd11b843
eloise <851113375@qq.com>
2017-02-16 14:17:10 +0800

    init

---
+++ group02/851113375/src/com/github/eloiseSJTU/coding2017/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.github.eloiseSJTU.coding2017;
+
+/**
+ * created by Rong on 2017-02-16
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+
+}
