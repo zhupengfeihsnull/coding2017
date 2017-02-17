2392f86564785f7899420b72f939e90053b1868c
Zhang34 <zz061945@sina.com>
2017-02-18 00:40:25 +0800

    init

---
+++ group02/328555902/src/com/github/zhang34/coding2017/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.github.zhang34.coding2017;
+
+/**
+ * Created by zhen on 2017/2/18.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub");
+    }
+}
